5c544d329f205f41341cdc84aff212b5e24a9519
cometscome <cometscome@gmail.com>
2022-01-07 19:21:36 +0900

    some change

--- src/TA_Gaugefields.jl
+++ src/TA_Gaugefields.jl
@@ -17,18 +17,13 @@ end
 
 
 function initialize_TA_Gaugefields(U::Array{<:AbstractGaugefields{NC,Dim},1}) where {NC,Dim}
-    F1 = initialize_TA_Gaugefields(U[1])
-    F = Array{typeof(F1),1}(undef,Dim)
-    F[1] = F1
-    for μ=2:Dim
+    F = Array{TA_Gaugefields{NC,Dim},1}(undef,Dim)
+    for μ=1:Dim
         F[μ] = initialize_TA_Gaugefields(U[μ])
     end
     return F
 end
 
-function initialize_TA_Gaugefields(u::AbstractGaugefields{NC,4}) where {NC}
-    return TA_Gaugefields_4D_serial(NC,u.NX,u.NY,u.NZ,u.NT)
-end
 
 function initialize_TA_Gaugefields(u::AbstractGaugefields{NC,Dim}) where {NC,Dim}
     mpi = u.mpi
@@ -43,9 +38,9 @@ function initialize_TA_Gaugefields(u::AbstractGaugefields{NC,Dim}) where {NC,Dim
         
     else
         if Dim==4
-            return TA_Gaugefields_4D_serial(NC,u.NX,u.NY,u.NZ,u.NT)
+            return TA_Gaugefields(NC,u.NX,u.NY,u.NZ,u.NT)
         elseif Dim==2
-            return TA_Gaugefields_2D_serial(NC,u.NX,u.NT)
+            return TA_Gaugefields(NC,u.NX,u.NT)
         else
             error("Dim = $Dim is not supoorted")
         end
