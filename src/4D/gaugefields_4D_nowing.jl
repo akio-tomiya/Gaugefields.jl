6ecb26c105c01dfdf8aec7aa33dc6218fd45b896
cometscome <cometscome@gmail.com>
2022-05-15 22:54:32 +0900

    staggered phase is revised

--- src/4D/gaugefields_4D_nowing.jl
+++ src/4D/gaugefields_4D_nowing.jl
@@ -467,8 +467,8 @@ using Random
         @inbounds return η*u.parent[i1,i2,i3,i4,i5,i6]
     end
 
-    function Base.getindex(u::Staggered_Gaugefields{Shifted_Gaugefields_4D_nowing{3},μ},i1,i2,i3,i4,i5,i6)  where {μ}
-        error("type $(typeof(U)) has no getindex method")
+    function Base.getindex(u::Staggered_Gaugefields{Shifted_Gaugefields_4D_nowing{NC},μ},i1,i2,i3,i4,i5,i6)  where {μ,NC}
+        error("type $(typeof(u)) has no getindex method")
     end
 
     function Base.getindex(u::Staggered_Gaugefields{T,μ},i1,i2,i3,i4,i5,i6) where {T <: Shifted_Gaugefields_4D,μ}
