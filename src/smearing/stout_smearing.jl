644d642019ac1bd2ce07ccf75fddb7216a752a78
Yuki Nagai <cometscome@gmail.com>
2024-05-16 03:10:48 -0400

    SU2

--- src/smearing/stout_smearing.jl
+++ src/smearing/stout_smearing.jl
@@ -1,6 +1,6 @@
 
 import ..AbstractGaugefields_module: clear_U!, add_U!, Gaugefields_4D_nowing, substitute_U!
-import ..AbstractGaugefields_module: calc_coefficients_Q
+import ..AbstractGaugefields_module: calc_coefficients_Q,calc_Bmatrix!
 
 mutable struct STOUTsmearing_layer{T,Dim,Tρ} <: CovLayer{Dim}
     ρs::Tρ#Vector{Tρ}
