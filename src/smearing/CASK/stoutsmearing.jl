9be8f39777d12ea593e088558cc1b1079c828941
cometscome <cometscome@gmail.com>
2024-06-23 02:29:15 +0200

    bugfix

--- src/smearing/CASK/stoutsmearing.jl
+++ src/smearing/CASK/stoutsmearing.jl
@@ -107,9 +107,9 @@ function backward_dSdUαUβρ_add!(s::STOUTsmearing_layer{T,Dim,Tρ}, dSdUα, dS
     dSdΩ = temps[3]
     dSdUdag = temps[4]
 
-    filterfunc(x) = ifelse(x > 0, 1, zero(x))
+    #filterfunc(x) = ifelse(x > 0, 1, zero(x))
     dNC = 0.1
-    #filterfunc(x) = ifelse(x > 0, (1 + x^2) * π / (2(NC + dNC)), zero(x))
+    filterfunc(x) = ifelse(x > 0, (1 + x^2) * π / (2(NC + dNC)), zero(x))
 
     #tempnew = similar(dSdUout)
     #dSdCs = temps[5:5+Dim-1]
