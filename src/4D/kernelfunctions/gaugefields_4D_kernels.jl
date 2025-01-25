7c983f4254a2ad18867984e507ac1ee1af30c14c
Yuki Nagai <cometscome@gmail.com>
2025-01-26 00:40:19 +0900

    kernel

--- src/4D/kernelfunctions/gaugefields_4D_kernels.jl
+++ src/4D/kernelfunctions/gaugefields_4D_kernels.jl
@@ -625,6 +625,200 @@ function kernel_clear_U!(b,r,c, NC)
     return
 end
 
+function kernel_exptU_TAwuww_NC3!(b,r,w,u,ww,t)
+    c1 = t * u[1,b,r] * 0.5
+    c2 = t * u[2,b,r] * 0.5
+    c3 = t * u[3,b,r] * 0.5
+    c4 = t * u[4,b,r] * 0.5
+    c5 = t * u[5,b,r] * 0.5
+    c6 = t * u[6,b,r] * 0.5
+    c7 = t * u[7,b,r] * 0.5
+    c8 = t * u[8,b,r] * 0.5
+    csum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8
+    if csum == 0
+        w[1, 1,b,r] = 1
+        w[1, 2,b,r] = 0
+        w[1, 3,b,r] = 0
+        w[2, 1,b,r] = 0
+        w[2, 2,b,r] = 1
+        w[2, 3,b,r] = 0
+        w[3, 1,b,r] = 0
+        w[3, 2,b,r] = 0
+        w[3, 3,b,r] = 1
+
+        ww[1, 1,b,r] = 1
+        ww[1, 2,b,r] = 0
+        ww[1, 3,b,r] = 0
+        ww[2, 1,b,r] = 0
+        ww[2, 2,b,r] = 1
+        ww[2, 3,b,r] = 0
+        ww[3, 1,b,r] = 0
+        ww[3, 2,b,r] = 0
+        ww[3, 3,b,r] = 1
+        return
+    end
+
+
+    #x[1,1,icum] =  c3+sr3i*c8 +im*(  0.0 )
+    v1 = c3 + sr3i * c8
+    v2 = 0.0
+    #x[1,2,icum] =  c1         +im*( -c2   )
+    v3 = c1
+    v4 = -c2
+    #x[1,3,icum] =  c4         +im*(-c5   )
+    v5 = c4
+    v6 = -c5
+
+    #x[2,1,icum] =  c1         +im*(  c2   )
+    v7 = c1
+    v8 = c2
+
+    #x[2,2,icum] =  -c3+sr3i*c8+im*(  0.0 )
+    v9 = -c3 + sr3i * c8
+    v10 = 0.0
+
+    #x[2,3,icum] =  c6         +im*( -c7   )
+    v11 = c6
+    v12 = -c7
+
+    #x[3,1,icum] =  c4         +im*(  c5   )
+    v13 = c4
+    v14 = c5
+
+    #x[3,2,icum] =  c6         +im*(  c7   )
+    v15 = c6
+    v16 = c7
+    #x[3,3,icum] =  -sr3i2*c8  +im*(  0.0 )
+    v17 = -sr3i2 * c8
+    v18 = 0.0
+
+
+    #c find eigenvalues of v
+    trv3 = (v1 + v9 + v17) / 3.0
+    cofac =
+        v1 * v9 - v3^2 - v4^2 + v1 * v17 - v5^2 - v6^2 + v9 * v17 - v11^2 -
+        v12^2
+    det =
+        v1 * v9 * v17 - v1 * (v11^2 + v12^2) - v9 * (v5^2 + v6^2) -
+        v17 * (v3^2 + v4^2) +
+        (v5 * (v3 * v11 - v4 * v12) + v6 * (v3 * v12 + v4 * v11)) * 2.0
+
+    p3 = cofac / 3.0 - trv3^2
+    q = trv3 * cofac - det - 2.0 * trv3^3
+    x = sqrt(-4.0 * p3) + tinyvalue
+
+    arg = q / (x * p3)
+
+    arg = min(1, max(-1, arg))
+    theta = acos(arg) / 3.0
+    e1 = x * cos(theta) + trv3
+    theta = theta + pi23
+    e2 = x * cos(theta) + trv3
+    #       theta = theta + pi23
+    #       e3 = x * cos(theta) + trv3
+    e3 = 3.0 * trv3 - e1 - e2
+
+    # solve for eigenvectors
+
+    w1 = v5 * (v9 - e1) - v3 * v11 + v4 * v12
+    w2 = -v6 * (v9 - e1) + v4 * v11 + v3 * v12
+    w3 = (v1 - e1) * v11 - v3 * v5 - v4 * v6
+    w4 = -(v1 - e1) * v12 - v4 * v5 + v3 * v6
+    w5 = -(v1 - e1) * (v9 - e1) + v3^2 + v4^2
+    w6 = 0.0
+    #println("1c $w1 $w2 $w3 $w4 $w5 $w6 ",)
+    #coeffv = sqrt(w1^2 + w2^2 + w3^2 + w4^2 + w5^2)
+
+    #coeff = ifelse(coeffv == zero(coeffv),0,coeffv)
+    coeff = 1.0 / sqrt(w1^2 + w2^2 + w3^2 + w4^2 + w5^2)
+    #println("1 ",coeff)
+
+    w1 = w1 * coeff
+    w2 = w2 * coeff
+    w3 = w3 * coeff
+    w4 = w4 * coeff
+    w5 = w5 * coeff
+
+    w7 = v5 * (v9 - e2) - v3 * v11 + v4 * v12
+    w8 = -v6 * (v9 - e2) + v4 * v11 + v3 * v12
+    w9 = (v1 - e2) * v11 - v3 * v5 - v4 * v6
+    w10 = -(v1 - e2) * v12 - v4 * v5 + v3 * v6
+    w11 = -(v1 - e2) * (v9 - e2) + v3^2 + v4^2
+    w12 = 0.0
+
+    coeff = 1.0 / sqrt(w7^2 + w8^2 + w9^2 + w10^2 + w11^2)
+
+    w7 = w7 * coeff
+    w8 = w8 * coeff
+    w9 = w9 * coeff
+    w10 = w10 * coeff
+    w11 = w11 * coeff
+
+    w13 = v5 * (v9 - e3) - v3 * v11 + v4 * v12
+    w14 = -v6 * (v9 - e3) + v4 * v11 + v3 * v12
+    w15 = (v1 - e3) * v11 - v3 * v5 - v4 * v6
+    w16 = -(v1 - e3) * v12 - v4 * v5 + v3 * v6
+    w17 = -(v1 - e3) * (v9 - e3) + v3^2 + v4^2
+    w18 = 0.0
+
+    coeff = 1.0 / sqrt(w13^2 + w14^2 + w15^2 + w16^2 + w17^2)
+    w13 = w13 * coeff
+    w14 = w14 * coeff
+    w15 = w15 * coeff
+    w16 = w16 * coeff
+    w17 = w17 * coeff
+
+    # construct the projection v
+    c1 = cos(e1)
+    s1 = sin(e1)
+    ww1 = w1 * c1 - w2 * s1
+    ww2 = w2 * c1 + w1 * s1
+    ww3 = w3 * c1 - w4 * s1
+    ww4 = w4 * c1 + w3 * s1
+    ww5 = w5 * c1 - w6 * s1
+    ww6 = w6 * c1 + w5 * s1
+
+    c2 = cos(e2)
+    s2 = sin(e2)
+    ww7 = w7 * c2 - w8 * s2
+    ww8 = w8 * c2 + w7 * s2
+    ww9 = w9 * c2 - w10 * s2
+    ww10 = w10 * c2 + w9 * s2
+    ww11 = w11 * c2 - w12 * s2
+    ww12 = w12 * c2 + w11 * s2
+
+    c3 = cos(e3)
+    s3 = sin(e3)
+    ww13 = w13 * c3 - w14 * s3
+    ww14 = w14 * c3 + w13 * s3
+    ww15 = w15 * c3 - w16 * s3
+    ww16 = w16 * c3 + w15 * s3
+    ww17 = w17 * c3 - w18 * s3
+    ww18 = w18 * c3 + w17 * s3
+
+    w[1, 1,b,r] = w1 + im * w2
+    w[1, 2,b,r] = w3 + im * w4
+    w[1, 3,b,r] = w5 + im * w6
+    w[2, 1,b,r] = w7 + im * w8
+    w[2, 2,b,r] = w9 + im * w10
+    w[2, 3,b,r] = w11 + im * w12
+    w[3, 1,b,r] = w13 + im * w14
+    w[3, 2,b,r] = w15 + im * w16
+    w[3, 3,b,r] = w17 + im * w18
+
+    ww[1, 1,b,r] = ww1 + im * ww2
+    ww[1, 2,b,r] = ww3 + im * ww4
+    ww[1, 3,b,r] = ww5 + im * ww6
+    ww[2, 1,b,r] = ww7 + im * ww8
+    ww[2, 2,b,r] = ww9 + im * ww10
+    ww[2, 3,b,r] = ww11 + im * ww12
+    ww[3, 1,b,r] = ww13 + im * ww14
+    ww[3, 2,b,r] = ww15 + im * ww16
+    ww[3, 3,b,r] = ww17 + im * ww18
+
+    return
+end
+
 function kernel_exptU_wvww!(b,r,w, v, ww, t, NC)
     v11 = v[1, 1, b, r]
     v22 = v[2, 2, b, r]
