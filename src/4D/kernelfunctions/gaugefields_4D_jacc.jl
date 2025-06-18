757f2742e050d5c31603c38065047e591352bc53
cometscome <cometscome@gmail.com>
2025-06-18 16:37:52 +0900

    randomize

--- src/4D/kernelfunctions/gaugefields_4D_jacc.jl
+++ src/4D/kernelfunctions/gaugefields_4D_jacc.jl
@@ -211,7 +211,7 @@ end
 
 function randomize_U!(c::Gaugefields_4D_accelerator{NC,TU,TUv,:jacc,TS}) where {NC,TU,TUv,TS}
     N = c.NX * c.NY * c.NZ * c.NT
-    ccpu = Gaugefields_4D_nowing(c.NX, c.NY, c.NZ, c.NT)
+    ccpu = Gaugefields_4D_nowing(NC, c.NX, c.NY, c.NZ, c.NT)
     #substitute_U!(ccpu, c)
     randomize_U!(ccpu)
     subsitute_U!(c, ccpu)
